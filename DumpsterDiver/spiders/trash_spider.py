9ce93f696b68105453b6ffe10a21953820dea466
Reakain <reakain@users.noreply.github.com>
2019-12-03 17:04:49 -0500

    We have functioning link scraping!

--- DumpsterDiver/spiders/trash_spider.py
+++ DumpsterDiver/spiders/trash_spider.py
@@ -2,20 +2,18 @@
 import scrapy
 
 
-class TrashSpiderSpider(scrapy.Spider):
+class TrashSpider(scrapy.Spider):
     name = 'trash_spider'
-    
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+    start_urls = [
+            'http://quotes.toscrape.com/'
         ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
+    visited_urls = ['http://quotes.toscrape.com/']
+    
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for new_url in response.css('a::attr(href)'):
+            if new_url is not None:
+                self.log('Found new url %s' % new_url)
+                if new_url not in self.visited_urls:
+                    self.visited_urls.append(new_url)
+                    yield response.follow(new_url, callback=self.parse)
+
